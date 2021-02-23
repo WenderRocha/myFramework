9b1a056b5186928e4d31f103df2e166b01ddc9ff
Wender Rocha <Wender_Dev@hotmail.com>
2021-02-23 12:19:43 -0300

    definindo layout view

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -2,13 +2,25 @@
 
 namespace App\Controllers;
 
-class IndexController
+use MF\Controller\Action;
+
+class IndexController extends Action
 {
-    public function index(){
-        echo "chegamos ao Indexcontroler e disparamos a action index";
+
+    public function index()
+    {
+        $this->view->dados = array(
+            'titulo' => 'meu site',
+            'nome' => 'joao da silva'
+        );
+
+        $this->render('index', 'layout1');
     }
 
-    public function  sobre_nos(){
-        echo "chegamos ao Indexcontroler e disparamos a action sobre_nos";
+    public function sobre_nos()
+    {
+        $this->render('sobreNos', 'layout2');
     }
+
+
 }
